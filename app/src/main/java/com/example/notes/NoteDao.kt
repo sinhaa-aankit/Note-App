0b4b057780fd959620c83d44f8e9383de1739bec
Ankit Kumar Sinha <40895035+sinhaa-aankit@users.noreply.github.com>
2020-11-15 19:10:19 +0530

    Created an interface to implement DAO (data access object). Specified SQL queries and specified them with method calls.

---
+++ app/src/main/java/com/example/notes/NoteDao.kt
@@ -0,0 +1,16 @@
+package com.example.notes
+
+import androidx.room.*
+
+@Dao
+interface NoteDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insert(note: Note)
+
+    @Delete
+    fun delete(note: Note)
+
+    @Query(value = "Select * from notes_table order by id ASC")
+    fun getAllNotes(): List<Note>
+}
