570f99c76bfb80d10c8745d7513dd00e44c060f1
Ankit Kumar Sinha <40895035+sinhaa-aankit@users.noreply.github.com>
2020-11-20 21:52:24 +0530

    Gave id to recyclerView. Implemented Adapter for recyclerView & also implemented it in main activity. Also handeled onclick of delete button to delete clicked note

--- app/src/main/java/com/example/notes/MainActivity.kt
+++ app/src/main/java/com/example/notes/MainActivity.kt
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), INotesRVAdapter {
 
     lateinit var viewModel: NoteViewModel
 
@@ -13,12 +15,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        val adapter = NotesRVAdapter(this, this)
+        recyclerView.adapter = adapter
+
         viewModel = ViewModelProvider(this,
             ViewModelProvider.AndroidViewModelFactory.getInstance(application)).get(NoteViewModel::class.java)
 
-        viewModel.allNotes.observe(this, Observer {
-            
+        viewModel.allNotes.observe(this, Observer {list ->
+            list?.let {
+                adapter.updateList(it)
+            }
         })
 
+
+    }
+
+    override fun onItemClicked(note: Note) {
+        viewModel.deleteNote(note)
     }
 }
--- app/src/main/java/com/example/notes/NoteViewModel.kt
+++ app/src/main/java/com/example/notes/NoteViewModel.kt
@@ -3,13 +3,22 @@ package com.example.notes
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class NoteViewModel(application: Application): AndroidViewModel(application) {
+
+    private val repository: NoteRepository
     val allNotes: LiveData<List<Note>>
 
     init {
         val dao = NoteDatabase.getDatabase(application).getNoteDao()
-        val repository = NoteRepository(dao)
+        repository = NoteRepository(dao)
         allNotes = repository.allNotes
+    }
+
+    fun deleteNote(note:Note) = viewModelScope.launch(Dispatchers.IO) {
+        repository.delete(note)
     }
 }
